6cefc3c05aad15db91ce9bfbc8eacd591187f62b
Sanjay C <sanjaycs483@gmail.com>
2026-02-11 20:49:58 +0530

    product and sum of all digit

--- src/Conditionals_and_loops/Basics/FibonacciSeries.java
+++ src/Conditionals_and_loops/Basics/FibonacciSeries.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class FibonacciSeries {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int n = in.nextInt();
         int a = 0;
         int b = 1;
---
+++ src/Conditionals_and_loops/Basics/ProductAndSum.java
@@ -0,0 +1,19 @@
+package Conditionals_and_loops.Basics;
+import java.util.Scanner;
+public class ProductAndSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int sum = 0;
+        int prod = 1;
+        while(n > 0){
+            int last = n%10;
+            sum += last;
+            prod *= last;
+            n = n/10;
+        }
+        int result = sum - prod;
+        System.out.println(result);
+    }
+}
